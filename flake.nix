f55f8572ef52e7387d3a5ee1922fbff1500e04c0
Said Rodrigues <coderflemis@gmail.com>
2024-02-28 20:33:24 -0300

    feat: add base setup

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A flake for develop a compiler";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ mdbook mdbook-mermaid jdk21 gradle ];
+        };
+      });
+}
