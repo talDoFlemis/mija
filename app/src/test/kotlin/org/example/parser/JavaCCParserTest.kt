1dfdfc9df7d0fc25cdcd36799d59a179e684acd9
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2024-03-17 18:18:06 -0300

    feat: add ProgramBuilder

--- app/src/test/kotlin/org/example/parser/JavaCCParserTest.kt
+++ app/src/test/kotlin/org/example/parser/JavaCCParserTest.kt
@@ -1,10 +1,20 @@
 package org.example.parser
 
+import org.example.factories.ProgramBuilder
+import org.example.factories.TokenFactory
 import org.junit.jupiter.api.Test
 
 class JavaCCParserTest {
     companion object {
         val parser = JavaCCParser()
+    }
+
+    val program = with(ProgramBuilder()) {
+        classDeclaration("Factorial") {
+            methodDeclaration("main", "static void", "String[] a") {
+                statement("System.out.println(7)")
+            }
+        }
     }
 
     @Test
@@ -31,13 +41,9 @@ class JavaCCParserTest {
     fun `Should check the syntax return error`() {
         with(JavaCCContext) {
             // Arrange
-            val code = """
-                | class Factorial{
-                |        public static void main(String[] a){
-                |            System.out.println(7)
-                |        } 
-                |}
-                """.trimMargin()
+            val code = program.build()
+                .trimMargin()
+                .also { println(it) }
 
             // Act
             val result = parser.checkSyntax(code)
