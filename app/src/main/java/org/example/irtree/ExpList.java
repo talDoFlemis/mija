61faa76addd6273daf0552c5b6732803d64a23d3
Antônio Gabriel <antgabriel@alu.ufc.br>
2024-05-01 12:02:23 -0300

    feature(#36): Creating test file

--- app/src/main/java/org/example/irtree/ExpList.java
+++ app/src/main/java/org/example/irtree/ExpList.java
@@ -1,11 +1,13 @@
 package org.example.irtree;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Builder
 @NoArgsConstructor
 public class ExpList {
     public ExpAbstract head;
--- app/src/test/java/org/example/visitor/irtree/IRTreeVisitorTest.java
+++ app/src/test/java/org/example/visitor/irtree/IRTreeVisitorTest.java
@@ -1,14 +1,21 @@
 package org.example.visitor.irtree;
 
 import org.example.ast.*;
+import org.example.irtree.BINOP;
+import org.example.irtree.CONST;
+import org.example.irtree.ExpAbstract;
 import org.example.mips.MipsFrame;
 import org.example.visitor.symbols.SymbolTableVisitor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 class IRTreeVisitorTest {
     static MainClass mockedMainClass() {
@@ -19,6 +26,95 @@ class IRTreeVisitorTest {
                     add(new Sout(new IntegerLiteral(1)));
                 }}))
                 .build();
+    }
+
+    static Stream<Arguments> shouldCheckForASameList() {
+        return Stream.of(
+                Arguments.of(
+                        BINOP.class,
+                        ClassDeclSimple.builder()
+                                .className(new Identifier("TubiasWithMethodLocal"))
+                                .methods(new MethodDeclList(new ArrayList<>() {{
+                                    add(MethodDecl.builder()
+                                            .identifier("main")
+                                            .formals(new FormalList(new ArrayList<>()))
+                                            .varDecls(new VarDeclList(new ArrayList<>() {{
+                                                add(VarDecl.builder().name("x").type(new IntegerType()).build());
+                                            }}))
+                                            .statements(new StatementList(new ArrayList<>() {{
+                                                add(new Sout(new IdentifierExpression("x")));
+                                            }}))
+                                            .type(new IntegerType())
+                                            .returnExpression(new IntegerLiteral(1))
+                                            .build());
+                                }}))
+                                .build()
+                ),
+                Arguments.of(
+                        BINOP.class,
+                        ClassDeclSimple.builder()
+                                .className(new Identifier("TubiasWithMethodParams"))
+                                .methods(new MethodDeclList(new ArrayList<>() {{
+                                    add(MethodDecl.builder()
+                                            .identifier("main")
+                                            .formals(new FormalList(new ArrayList<>() {{
+                                                add(Formal.builder().name("x").type(new IntegerType()).build());
+                                            }}))
+                                            .varDecls(new VarDeclList(new ArrayList<>()))
+                                            .statements(new StatementList(new ArrayList<>() {{
+                                                add(new Sout(new IdentifierExpression("x")));
+                                            }}))
+                                            .type(new IntegerType())
+                                            .returnExpression(new IntegerLiteral(1))
+                                            .build());
+                                }}))
+                                .build()
+                ),
+                Arguments.of(
+                        BINOP.class,
+                        ClassDeclSimple.builder()
+                                .className(new Identifier("TubiasWithClassFields"))
+                                .fields(new VarDeclList(new ArrayList<>() {{
+                                    add(VarDecl.builder().name("x").type(new IntegerType()).build());
+                                }}))
+                                .methods(new MethodDeclList(new ArrayList<>() {{
+                                    add(MethodDecl.builder()
+                                            .identifier("main")
+                                            .formals(new FormalList(new ArrayList<>()))
+                                            .varDecls(new VarDeclList(new ArrayList<>()))
+                                            .statements(new StatementList(new ArrayList<>() {{
+                                                add(new Sout(new IdentifierExpression("x")));
+                                            }}))
+                                            .type(new IntegerType())
+                                            .returnExpression(new IntegerLiteral(1))
+                                            .build());
+                                }}))
+                                .build()
+                ),
+                Arguments.of(
+                        BINOP.class,
+                        ClassDeclSimple.builder()
+                                .className(new Identifier("TubiasWithClassFields"))
+                                .methods(new MethodDeclList(new ArrayList<>() {{
+                                    add(MethodDecl.builder()
+                                            .identifier("main")
+                                            .formals(new FormalList(new ArrayList<>()))
+                                            .varDecls(new VarDeclList(new ArrayList<>() {{
+                                                add(VarDecl.builder().name("x").type(new IdentifierType(mockedMainClass().getClassName().getS())).build());
+                                            }}))
+                                            .statements(new StatementList(new ArrayList<>() {{
+                                                add(Assign.builder()
+                                                        .identifier(new Identifier("x"))
+                                                        .value(new NewObject(new Identifier(mockedMainClass().getClassName().getS())))
+                                                        .build());
+                                            }}))
+                                            .type(new IntegerType())
+                                            .returnExpression(new IntegerLiteral(1))
+                                            .build());
+                                }}))
+                                .build()
+                )
+        );
     }
 
     @Test
@@ -52,11 +148,42 @@ class IRTreeVisitorTest {
         IRTreeVisitor irTreeVisitor = new IRTreeVisitor(symbolTableVisitor.getMainTable(), new MipsFrame());
 
         // ACT
+        var teste = CONST.builder().value(1).build();
+
+        prog.accept(irTreeVisitor);
+
+        // ASSERT
+        assertFalse(irTreeVisitor.getListExp().isEmpty());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Should check for a equals list")
+    @MethodSource
+    void shouldCheckForASameList(Class<? extends ExpAbstract> expected, ClassDeclSimple classDecl) {
+        // ARRANGE
+        Program prog = Program.builder()
+                .mainClass(mockedMainClass())
+                .classes(new ClassDeclList(new ArrayList<>() {{
+                    add(classDecl);
+                }}))
+                .build();
+        SymbolTableVisitor symbolTableVisitor = new SymbolTableVisitor();
+        prog.accept(symbolTableVisitor);
+        IRTreeVisitor irTreeVisitor = new IRTreeVisitor(symbolTableVisitor.getMainTable(), new MipsFrame());
+
+        // ACT
         prog.accept(irTreeVisitor);
 
         System.out.println(irTreeVisitor.getListExp());
 
         // ASSERT
-        assertFalse(irTreeVisitor.getListExp().isEmpty());
+
+        for (var item : irTreeVisitor.getListExp()) {
+            if (expected.isInstance(item)) {
+                assertTrue(true);
+                return;
+            }
+        }
+        fail();
     }
 }
