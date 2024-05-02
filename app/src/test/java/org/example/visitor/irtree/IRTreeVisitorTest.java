af5c6148e92670a04ce772e855bb4d43be2fa6c8
Said Rodrigues <coderflemis@gmail.com>
2024-05-02 15:19:09 -0300

    tests(#36): cleanup shit tests

--- app/src/test/java/org/example/visitor/irtree/IRTreeVisitorTest.java
+++ app/src/test/java/org/example/visitor/irtree/IRTreeVisitorTest.java
@@ -28,95 +28,6 @@ class IRTreeVisitorTest {
                 .build();
     }
 
-    static Stream<Arguments> shouldCheckForASameList() {
-        return Stream.of(
-                Arguments.of(
-                        BINOP.class,
-                        ClassDeclSimple.builder()
-                                .className(new Identifier("TubiasWithMethodLocal"))
-                                .methods(new MethodDeclList(new ArrayList<>() {{
-                                    add(MethodDecl.builder()
-                                            .identifier("main")
-                                            .formals(new FormalList(new ArrayList<>()))
-                                            .varDecls(new VarDeclList(new ArrayList<>() {{
-                                                add(VarDecl.builder().name("x").type(new IntegerType()).build());
-                                            }}))
-                                            .statements(new StatementList(new ArrayList<>() {{
-                                                add(new Sout(new IdentifierExpression("x")));
-                                            }}))
-                                            .type(new IntegerType())
-                                            .returnExpression(new IntegerLiteral(1))
-                                            .build());
-                                }}))
-                                .build()
-                ),
-                Arguments.of(
-                        BINOP.class,
-                        ClassDeclSimple.builder()
-                                .className(new Identifier("TubiasWithMethodParams"))
-                                .methods(new MethodDeclList(new ArrayList<>() {{
-                                    add(MethodDecl.builder()
-                                            .identifier("main")
-                                            .formals(new FormalList(new ArrayList<>() {{
-                                                add(Formal.builder().name("x").type(new IntegerType()).build());
-                                            }}))
-                                            .varDecls(new VarDeclList(new ArrayList<>()))
-                                            .statements(new StatementList(new ArrayList<>() {{
-                                                add(new Sout(new IdentifierExpression("x")));
-                                            }}))
-                                            .type(new IntegerType())
-                                            .returnExpression(new IntegerLiteral(1))
-                                            .build());
-                                }}))
-                                .build()
-                ),
-                Arguments.of(
-                        BINOP.class,
-                        ClassDeclSimple.builder()
-                                .className(new Identifier("TubiasWithClassFields"))
-                                .fields(new VarDeclList(new ArrayList<>() {{
-                                    add(VarDecl.builder().name("x").type(new IntegerType()).build());
-                                }}))
-                                .methods(new MethodDeclList(new ArrayList<>() {{
-                                    add(MethodDecl.builder()
-                                            .identifier("main")
-                                            .formals(new FormalList(new ArrayList<>()))
-                                            .varDecls(new VarDeclList(new ArrayList<>()))
-                                            .statements(new StatementList(new ArrayList<>() {{
-                                                add(new Sout(new IdentifierExpression("x")));
-                                            }}))
-                                            .type(new IntegerType())
-                                            .returnExpression(new IntegerLiteral(1))
-                                            .build());
-                                }}))
-                                .build()
-                ),
-                Arguments.of(
-                        BINOP.class,
-                        ClassDeclSimple.builder()
-                                .className(new Identifier("TubiasWithClassFields"))
-                                .methods(new MethodDeclList(new ArrayList<>() {{
-                                    add(MethodDecl.builder()
-                                            .identifier("main")
-                                            .formals(new FormalList(new ArrayList<>()))
-                                            .varDecls(new VarDeclList(new ArrayList<>() {{
-                                                add(VarDecl.builder().name("x").type(new IdentifierType(mockedMainClass().getClassName().getS())).build());
-                                            }}))
-                                            .statements(new StatementList(new ArrayList<>() {{
-                                                add(Assign.builder()
-                                                        .identifier(new Identifier("x"))
-                                                        .value(new NewObject(new Identifier(mockedMainClass().getClassName().getS())))
-                                                        .build());
-                                            }}))
-                                            .type(new IntegerType())
-                                            .returnExpression(new IntegerLiteral(1))
-                                            .build());
-                                }}))
-                                .build()
-                )
-        );
-    }
-
     @Test
     @DisplayName("Should check a non empty list of expression")
     void shouldCheckANonEmptyListOfExpression() {
@@ -153,37 +64,6 @@ class IRTreeVisitorTest {
 
         // ASSERT
         assertFalse(irTreeVisitor.getListExp().isEmpty());
-    }
-
-    @ParameterizedTest
-    @DisplayName("Should check for a equals list")
-    @MethodSource
-    void shouldCheckForASameList(Class<? extends ExpAbstract> expected, ClassDeclSimple classDecl) {
-        // ARRANGE
-        Program prog = Program.builder()
-                .mainClass(mockedMainClass())
-                .classes(new ClassDeclList(new ArrayList<>() {{
-                    add(classDecl);
-                }}))
-                .build();
-        SymbolTableVisitor symbolTableVisitor = new SymbolTableVisitor();
-        prog.accept(symbolTableVisitor);
-        IRTreeVisitor irTreeVisitor = new IRTreeVisitor(symbolTableVisitor.getMainTable(), new MipsFrame());
-
-        // ACT
-        prog.accept(irTreeVisitor);
-
-        System.out.println(irTreeVisitor.getListExp());
-
-        // ASSERT
-
-        for (var item : irTreeVisitor.getListExp()) {
-            if (expected.isInstance(item)) {
-                assertTrue(true);
-                return;
-            }
-        }
-        fail();
     }
 
     static Stream<Arguments> shouldParseBinaryAndUnaryOperations() {
